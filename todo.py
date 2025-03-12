d904e582b6201aa93fa495118ff01ee2939f4e1e
Zeamanuel-Admasu <zeamanshibru2020@gmail.com>
2025-03-12 23:25:05 +0300

    Implemented mark_completed() method with initial version

--- todo.py
+++ todo.py
@@ -20,5 +20,14 @@ class ToDoList:
             print(f"Task removed: {removed_task}")
         else:
             print("Invalid task index.")
+    def Mark_completed(self, task_index):
+        if 0 <= task_index < len(self.tasks):
+            if self.tasks[task_index]["completed"]:
+                print(f"Task '{self.tasks[task_index]['task']}' is already completed.")
+            else:
+                self.tasks[task_index]["completed"] = True
+                print(f"Task marked as completed: {self.tasks[task_index]['task']}")
+        else:
+            print("Invalid task index.")
 
 
